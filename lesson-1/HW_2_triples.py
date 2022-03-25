9c7caf5a53c7c85f54be3c2c4b149dc08c67bd55
paha85 <padisb@gmail.com>
2022-03-25 23:44:01 +0300

    Перечитал задание, в начале думал, что 17 надо прибавлять ко второму списку.

--- lesson-1/HW_2_triples.py
+++ lesson-1/HW_2_triples.py
@@ -1,22 +1,23 @@
 list_1 = []
 for num_1 in range(1, 1001, 2):
     list_1.append(num_1 ** 3)
-print("Список из кубов нечетных чисел от 1 до 1000: ", list_1)
+# print("Список из кубов нечетных чисел от 1 до 1000: ", list_1)
 
 list_2 = []
-for num_2 in list_1:
+for num in list_1:
     sum_1 = 0
-    for num_3 in str(num_2):
-        sum_1 += int(num_3)
+    for index in range(len(str(num))):
+        sum_1 += int(str(num)[index])
     if sum_1 % 7 == 0:
-        list_2.append(num_2 + 17)
-print('Список чисел сумма цифр которых делится на 7 без остатка, к которым прибавили 17: ', list_2)
+        list_2.append(num)
+print('Числа, чья сумма цифр делится на 7 без остатка: ', list_2)
 
 list_3 = []
-for num_4 in list_2:
+for num_1 in list_1:
+    num_1 += 17
     sum_2 = 0
-    for num_5 in str(num_4):
-        sum_2 += int(num_5)
+    for index in range(len(str(num_1))):
+        sum_2 += int(str(num_1)[index])
     if sum_2 % 7 == 0:
-        list_3.append(num_4)
-print('Список чисел из второго списка, сумма цифр которых делится на 7 без остатка : ', list_3)
+        list_3.append(num_1)
+print('Числа, к которым прибавили 17, чья сумма цифр делится на 7 без остатка: ', list_3)
